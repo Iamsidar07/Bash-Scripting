28d87dc72da1082268d51224bab64aefb70428b7
Manoj Kumar <ms8460149@gmail.com>
2024-01-27 15:05:20 +0530

    automated workflow

---
+++ open_links.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+xdg-open "https://twitter.com/Iamsidar07"
+xdg-open "https://www.udemy.com/"
+xdg-open "https://github.com/Iamsidar07"
+xdg-open "https://monkeytype.com/"
+/usr/bin/code
